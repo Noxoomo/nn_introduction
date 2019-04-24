6ec00bb8a92b16771b38df31ea8d44d08eff8b2c
towelenee <belous.mikhail@gmail.com>
2019-04-24 13:27:34 +0300

    localfix

--- cpp/funcs/ut/soft_tree_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <models/polynom/polynom_autograd_gpu.h>
-#include <gtest/gtest.h>
-
-TEST(A, SoftTreeCuda) {
-    EXPECT_EQ(2 * 2, 4);
-    SoftTreeCuda softTreeCuda;
-    softTreeCuda.testy_test();
-}
